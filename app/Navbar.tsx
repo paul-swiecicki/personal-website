a04359ac653c48a9740afbcae5d00e3d58e00b6f
paul-swiecicki <paul.swiecicki2@gmail.com>
2023-02-25 21:54:50 +0100

    create separate links file

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,14 +1,8 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import logo from '../public/ps-logo.svg'
+import { links } from '../data/links'
 import './Navbar.scss'
-
-const links = {
-  home: 'Home',
-  work: 'My work',
-  skills: 'My skills',
-  contact: 'Contact',
-}
 
 function NavLink({ children, href }: { children: string; href: string }) {
   return (
@@ -20,13 +14,11 @@ function NavLink({ children, href }: { children: string; href: string }) {
 
 export default function Navbar() {
   const mapLinks = (links: Record<string, string>): JSX.Element[] => {
-    return Object.entries(links).map(([path, name]) => {
-      return (
-        <NavLink key={path} href={'#' + path}>
-          {name}
-        </NavLink>
-      )
-    })
+    return Object.entries(links).map(([path, name]) => (
+      <NavLink key={path} href={'#' + path}>
+        {name}
+      </NavLink>
+    ))
   }
 
   return (
---
+++ data/links.ts
@@ -0,0 +1,8 @@
+const links: Record<string, string> = {
+  home: 'Home',
+  work: 'My work',
+  skills: 'My skills',
+  contact: 'Contact',
+}
+
+export { links }
