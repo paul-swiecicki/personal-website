39b3be2357d48f51ad39c5e9b8946159e8e1bc21
paul-swiecicki <paul.swiecicki2@gmail.com>
2023-04-03 15:20:09 +0200

    add projects

--- app/Projects.tsx
+++ app/Projects.tsx
@@ -1,4 +1,6 @@
 import EP from '../public/EP.png'
+import AutoEtlog from '../public/auto-etlog.jpg'
+import BloonBot from '../public/bloonbot.png'
 import Project from './Project'
 import './Project.scss'
 
@@ -7,22 +9,76 @@ export default function Projects() {
     <section className="projects">
       <h2>My work</h2>
       <Project
+        title="This website"
+        imgSrc={EP}
+        tags={['frontend', 'design', 'web']}
+        techStack={[
+          'next',
+          'react',
+          'typescript',
+          'scss',
+          'javascript',
+          'html',
+          'css',
+        ]}
+        githubLink="https://github.com/paul-swiecicki/personal-website"
+      >
+        Be careful, recursion has been detected in the area!
+      </Project>
+
+      <Project
         title="Euro Papryka"
         imgSrc={EP}
-        tags={['frontend', 'design']}
+        tags={['frontend', 'design', 'web']}
         techStack={[
           'react',
           'gatsby',
           'styled components',
           'contentful',
-          'html',
           'javascript',
+          'html',
           'css',
         ]}
         liveLink="http://grupy.home.pl/strona/products"
       >
         Website for vegetable & fruit supplier. Features, among others, CMS
         integration (Contentful), internationalization and e-mail contact form.
+      </Project>
+
+      <Project
+        title="Auto EtLog"
+        imgSrc={AutoEtlog}
+        tags={['frontend', 'backend', 'desktop']}
+        techStack={['electron', 'robotjs', 'html', 'javascript', 'css']}
+        githubLink="https://github.com/paul-swiecicki/auto-etlog"
+      >
+        App built on Electron.js for the vegetable supplier that automates
+        printing logistical labels. Reduced the amount of manual work needed for
+        the task by about 70%.
+      </Project>
+
+      <Project
+        title="BloonBot"
+        imgSrc={BloonBot}
+        tags={['frontend', 'backend', 'web', 'design']}
+        techStack={[
+          'react',
+          'scss',
+          'python',
+          'node',
+          'express',
+          'mongodb',
+          'javascript',
+          'html',
+          'css',
+        ]}
+        githubLink="https://github.com/paul-swiecicki/bloonbot-website"
+        liveLink="https://paul-swiecicki.github.io/bloonbot-website/"
+      >
+        BloonBot is a highly customizable chatbot making use of RegEx built on
+        JavaScript that has its installer, file management, website (built using
+        React & SCSS), and backend with authentication allowing for saving
+        settings.
       </Project>
     </section>
   )
